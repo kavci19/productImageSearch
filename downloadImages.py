463cea525e58176384754f95e78272eec2258903
Kaan Avci <kaanyewest1@gmail.com>
2020-08-19 18:53:31 -0400

    added functionality to store possible images in separate folder, and list unfound queries

--- downloadImages.py
+++ downloadImages.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from google_images_download import google_images_download
 import csv
 
@@ -6,12 +7,18 @@ from numpy.core.defchararray import rfind
 
 response = google_images_download.googleimagesdownload()
 
-SAVE_FOLDER = 'Product_Images'
+unmatched_products = []
 
+SAVE_FOLDER = 'Product_Images'
+POSSIBLE_FOLDER = 'Possible_Matches'
 
 def main():
+
     if not os.path.exists(SAVE_FOLDER):
         os.mkdir(SAVE_FOLDER)
+
+    if not os.path.exists(POSSIBLE_FOLDER):
+        os.mkdir(POSSIBLE_FOLDER)
 
     scan_barcodes()
 
@@ -23,23 +30,36 @@ def scan_barcodes():
         line_count = 0
 
         for row in csv_reader:
+            print()
 
             if line_count == 0:
                 line_count += 1
                 continue
             else:
-                print("Scanning Item: " + str(line_count - 1))
+                if line_count==50:
+                    break
+
+                progress = (line_count/20000)*100
+
+                print(str(progress) + '% done')
+
                 barcode = row[1]
+                name = row[0]
                 
-                download_image(barcode)
-                line_count += 1
+                download_image(barcode, name)
+
+            line_count += 1
 
         print('Processed ' + str(line_count) + ' lines')
         print('Done')
+        print()
+        writeUnmatchedProducts()
+        print('Could not find images for ' + str(len(unmatched_products)) + ' queries. See Unmatched_Products CSV file for details.')
 
 
 
-def download_image(query):
+
+def download_image(barcode, name):
     # keywords is the search query
     # format is the image file format
     # limit is the number of images to be downloaded
@@ -49,15 +69,18 @@ def download_image(query):
     # aspect ratio denotes the height width ratio
     # of images to download. ("tall, square, wide, panoramic")
 
-    if "+" in query or "-" in query:
+    blockPrint()
+
+    if "+" in barcode or "-" in barcode:
+        try_download(barcode, name)
         return
 
     GOOGLE_IMAGE = \
         'https://www.google.com/search?site=&tbm=isch&source=hp&biw=1873&bih=990&'
 
-    searchurl = GOOGLE_IMAGE + 'q=' + query
+    searchurl = GOOGLE_IMAGE + 'q=' + barcode
 
-    arguments = {"keywords": query,
+    arguments = {"keywords": barcode,
                  "format": "jpg",
                  "limit": 1,
                  "print_urls": True,
@@ -66,7 +89,7 @@ def download_image(query):
                  "output_directory": SAVE_FOLDER,
                  "no_directory": True,
                  "url": searchurl,
-                 "name": query}
+                 "name": barcode}
 
     try:
 
@@ -74,22 +97,94 @@ def download_image(query):
         a = list(a[0].values())[0][0]
         lastIndex = a.rfind('\\')
         base = a[:lastIndex+1]
-        newName = base + str(query) + '.jpg'
+        newName = base + str(barcode) + '.jpg'
         os.rename(a, newName)
+        enablePrint()
 
     except SystemExit:
+        try_download(barcode, name)
+        enablePrint()
         return
 
     except IndexError:
+        try_download(barcode, name)
+        enablePrint()
         return
 
     except FileExistsError:
+        #try_download(barcode, name)
+        enablePrint()
         return
 
     except FileNotFoundError:
+        try_download(barcode, name)
+        enablePrint()
         return
 
 
+
+
+def try_download(barcode, name):
+
+    GOOGLE_IMAGE = \
+        'https://www.google.com/search?site=&tbm=isch&source=hp&biw=1873&bih=990&'
+
+    searchurl = GOOGLE_IMAGE + 'q=' + name
+
+    arguments = {"keywords": name,
+                 "format": "jpg",
+                 "limit": 1,
+                 "print_urls": True,
+                 "size": "large",
+                 "aspect_ratio": "panoramic",
+                 "output_directory": POSSIBLE_FOLDER,
+                 "no_directory": True,
+                 "url": searchurl,
+                 "name": name}
+
+    try:
+
+        a = response.download(arguments)
+        a = list(a[0].values())[0][0]
+        lastIndex = a.rfind('\\')
+        base = a[:lastIndex+1]
+        newName = base + str(barcode) + '.jpg'
+        os.rename(a, newName)
+
+    except SystemExit:
+        unmatched_products.append([name,barcode])
+        return
+
+    except IndexError:
+        unmatched_products.append([name,barcode])
+        return
+
+    except FileExistsError:
+        unmatched_products.append([name,barcode])
+        return
+
+    except FileNotFoundError:
+        unmatched_products.append([name,barcode])
+        return
+
+
+
+# Disable
+def blockPrint():
+    sys.stdout = open(os.devnull, 'w')
+
+# Restore
+def enablePrint():
+    sys.stdout = sys.__stdout__
+
+
+def writeUnmatchedProducts():
+
+    with open('Unmatched_Products.csv', mode='w') as unmatched:
+        writer = csv.writer(unmatched, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+        for product in unmatched_products:
+            writer.writerow(product)
 
 
 
